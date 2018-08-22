cf539e513013535abc2863b4102b8f6b4e7105d9
Christian Liebel <christian.liebel@thinktecture.com>
2018-08-22 13:48:37 +0200

    feat(database): remove ref

--- src/modules/shared/services/base/database.service.ts
+++ src/modules/shared/services/base/database.service.ts
@@ -1,10 +1,8 @@
 import {Injectable} from '@angular/core';
 import Dexie from 'dexie';
-import {DatabaseServiceRef} from '../database.service.ref';
 
 @Injectable({
     providedIn: 'root',
-    useClass: DatabaseServiceRef
 })
 export class DatabaseService extends Dexie {
     constructor() {
